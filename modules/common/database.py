5ca3cf07ac14df490898e5e643066b7b90c7dc80
Vlad Yevtushenko <mstoun69@gmail.com>
2023-02-26 17:11:31 +0200

    New test Database

--- modules/common/database.py
+++ modules/common/database.py
@@ -4,7 +4,7 @@ import sqlite3
 class Database():
 
     def __init__(self):
-        self.connection = sqlite3.connect(r'/e/GitHub/QAAuto23' + r'/become_qa_auto.db')
+        self.connection = sqlite3.connect('e:\\GitHub\\QAAuto23' + '\\become_qa_auto.db')
         self.cursor = self.connection.cursor()
 
     def test_connection(self):
@@ -12,3 +12,15 @@ class Database():
         self.cursor.execute(sqlite_select_Query)
         record = self.cursor.fetchall()
         print(f"Connected successfully. SQLite Database Version is: {record}")
+
+    def get_all_users(self):
+        query = "SELECT name, address, city FROM customers"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+
+    def get_user_address_by_name(self, name):
+        query = f"SELECT address, city, postalCode, country FROM customers WHERE name = '{name}'"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
